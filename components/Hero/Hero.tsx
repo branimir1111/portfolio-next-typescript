e78084f621fb34f579e2d83d2da7a02ac3fbd11e
Branimir Djordjevic <branimir1111@gmail.com>
2024-09-10 22:12:48 +0200

    completed first part of hero section

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,9 +1,11 @@
 import { motion } from 'framer-motion';
 import { HeroHighlight, Highlight } from '../ui/hero-highlight';
+import profileImage from '@/public/portfolioPicture.jpg';
+import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <HeroHighlight containerClassName="min-h-screen dark:bg-black-100">
+    <HeroHighlight containerClassName="min-h-screen grid place-items-center dark:bg-black-100">
       <motion.h1
         initial={{
           opacity: 0,
@@ -19,10 +21,30 @@ const Hero = () => {
         }}
         className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
       >
+        <div className="flex flex-col items-center gap-4 sm:flex-row sm:justify-center mb-8 sm:items-end">
+          <Image
+            src={profileImage}
+            alt="profile image"
+            className="w-12 h-12 rounded-md"
+          />
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            branimir <span className="text-purple">djordjevic</span>
+          </p>
+        </div>
         Developing Ideas Into Harmonious{' '}
         <Highlight className="text-black dark:text-white">
           User Interface
         </Highlight>
+        <p className="mt-8 text-center md:tracking-wider text-sm md:text-lg lg:text-2xl">
+          Let&apos;s create the future together.
+        </p>
+        <a
+          href="/BranimirDjordjevicCV.pdf"
+          target="__blank"
+          className="mb-6 md:mb-0 text-sm md:text-lg lg:text-xl text-violet-400 hover:text-violet-300 hover:font-bold transition-all duration-200"
+        >
+          Download CV
+        </a>
       </motion.h1>
     </HeroHighlight>
   );
