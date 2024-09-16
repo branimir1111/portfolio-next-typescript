4c70c67b5f14acf938cf670fcba213d425b3c501
Branimir Djordjevic <branimir1111@gmail.com>
2024-09-16 23:14:26 +0200

    yeyeye

---
+++ utils/data/aboutSectionData.tsx
@@ -0,0 +1,45 @@
+type AboutSectionDataProps = {
+  readonly id: number;
+  title: string;
+  description: string;
+};
+
+export const aboutSectionData: AboutSectionDataProps[] = [
+  {
+    id: 1,
+    title:
+      'Building strong client relationships through open communication is my priority',
+    description: '',
+  },
+  {
+    id: 2,
+    title: 'Frontend',
+    description: '',
+  },
+  {
+    id: 3,
+    title: 'Backend',
+    description: '',
+  },
+  {
+    id: 4,
+    title: "Let's connect no matter where you are in the world",
+    description: '',
+  },
+  {
+    id: 5,
+    title:
+      'A development-oriented technologist with professional focus on technological advancements',
+    description: '',
+  },
+  {
+    id: 6,
+    title: 'How about we start project as a team?',
+    description: '',
+  },
+  {
+    id: 7,
+    title: 'Currently working on a project management system',
+    description: 'The inside update',
+  },
+];
