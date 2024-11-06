7511ea47287e72a21ee098fc04f63f8e4925cb6a
Branimir Djordjevic <branimir1111@gmail.com>
2024-11-06 16:39:26 +0100

    refactor off timeline component

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -12,7 +12,7 @@ interface TimelineEntry {
   content: React.ReactNode;
 }
 
-const Timeline = ({ data }: { data: TimelineEntry[] }) => {
+export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
   const ref = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
   const [height, setHeight] = useState(0);
@@ -34,19 +34,16 @@ const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div
-      className="w-full bg-white dark:bg-black-100 font-sans md:px-10"
+      className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10"
       ref={containerRef}
     >
-      <div className="max-w-7xl mx-auto pt-20 px-4 md:px-8 lg:px-10">
+      <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
         <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
-          Projects
+          Changelog from my journey
         </h2>
-        <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base">
-          I have been working as a web developer for the past two and a half
-          years.
-        </p>
-        <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base">
-          Here are some of my projects.
+        <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
+          I&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
+          a timeline of my journey.
         </p>
       </div>
 
@@ -91,5 +88,3 @@ const Timeline = ({ data }: { data: TimelineEntry[] }) => {
     </div>
   );
 };
-
-export { Timeline };
